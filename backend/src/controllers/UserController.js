181975ba50437eee01e2874f900f05cb507b6353
will-io <wesquive@gmu.edu>
2020-10-28 19:55:39 -0400

    test

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken')
-//comment
+
 module.exports = {
     async createUser(req,res){
         try{
